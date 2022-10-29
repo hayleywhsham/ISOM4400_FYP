32f88ce2092dd0ba3882e6afeb903d53d8aefa7e
hayleysham <hayley11sham@gmail.com>
2022-10-29 17:20:54 +0800

    add scrapping function

--- get_url.py
+++ get_url.py
@@ -2,22 +2,39 @@ import bs4
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
+import time
 
 def main():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     link = "https://www.facebook.com/ViuTV.hk/"
     driver.get(link)
     html = driver.page_source
+    time.sleep(2)
     driver.close()
     soup = bs4.BeautifulSoup(html, "html.parser")
-    soup(text=lambda t: "http" in t.text)
+    urls = soup.find_all("a")
+    url_pool = []
 
-    url_pool = soup.find_all("a")
+    def is_http(text):
+        return "http" in text
 
-    for url in url_pool:
-    #    #if url and ('https' in url) and (not 'facebook' in url) and (not 'fbcdn' in url):
-        print(url.get_text())
+    def get_only_url(text):
+        start = text.find('http')
+        end = min(text[start:].find(" "), text[start:].find("\n"))
+        if end == -1:
+            end = len(text)
+        return text[start:end]
+
+    def is_unique(text):
+        return not (text in url_pool)
+
+    for url in urls:
+        if url.has_attr('href') and "http" in url.get_text():
+            lines = list(filter(is_http, url.get_text().split("\n")))
+            all_urls = list(map(get_only_url, lines))
+            unique_urls = list(filter(is_unique, all_urls))
+            url_pool.extend(unique_urls)
+    print(url_pool)
 
 if __name__ == "__main__":
     main()
---
+++ test_scraper.py
@@ -0,0 +1,33 @@
+from facebook_scraper import get_posts
+
+def get_only_url(text):
+    start = text.find('http')
+    if start == -1:
+        return "", -1
+    space_pos = text[start:].find(" ") + start if text[start:].find(" ") != -1 else len(text)
+    nextrow_pos = text[start:].find("\n") + start if text[start:].find("\n") != -1 else len(text)
+    end = min(space_pos,nextrow_pos)
+    return text[start:end], end
+
+def get_all_url(text):
+    urls = []
+    while len(text)>0:
+        url, end = get_only_url(text)
+        if len(url)<=0:
+            break
+        urls.append(url)
+        text = text[end:]
+        #print(text)
+        #print("="*10)
+    return urls
+
+
+def main():
+    #print(get_all_url("http:aaaaaa\n  and ahahha ahhaha http:wtf"))
+    for post in get_posts('SHISEIDOHK', pages=3):
+        print(post['time'])
+        print(get_all_url(post['text']))
+        print("==============")
+
+if __name__ == "__main__":
+    main()
