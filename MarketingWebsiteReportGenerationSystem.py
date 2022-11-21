b3f47c895e5f4c8cb683436f2efdfe72741dcad1
hayleysham <hayley11sham@gmail.com>
2022-11-21 23:49:19 +0800

    Set Up Development Environment

---
+++ MarketingWebsiteReportGenerationSystem.py
@@ -0,0 +1,31 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import sys
+from MainPageUI import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.stackedWidget.setCurrentWidget(self.ui.search_page)
+        #Connect Function: only connect once for each button
+        self.ui.button_search_page_search_marketing_sites.clicked.connect(self.test_change_page)
+
+    def init_link_page(self):
+        print("hi")
+
+    def test_change_page(self):
+        self.init_link_page()
+        self.ui.stackedWidget.setCurrentWidget(self.ui.links_page)
+
+
+def main():
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
