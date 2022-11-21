71a362c1fde354f32b3ae360b444a199b7b91d97
hayleysham <hayley11sham@gmail.com>
2022-11-21 23:50:34 +0800

    Update MarketingWebsiteReportGenerationSystem.py

--- MarketingWebsiteReportGenerationSystem.py
+++ MarketingWebsiteReportGenerationSystem.py
@@ -4,13 +4,14 @@ from PyQt5.QtCore import *
 import sys
 from MainPageUI import Ui_MainWindow
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.stackedWidget.setCurrentWidget(self.ui.search_page)
-        #Connect Function: only connect once for each button
+        # Connect Function: only connect once for each button
         self.ui.button_search_page_search_marketing_sites.clicked.connect(self.test_change_page)
 
     def init_link_page(self):
@@ -26,6 +27,7 @@ def main():
     main_window = MainWindow()
     main_window.show()
     sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
     main()
