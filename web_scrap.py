67ea6e796512fc200ef2dc5dc9fdb4089dead2e4
hugoho82 <hohugo56@gmail.com>
2022-12-30 04:07:26 +0800

    Revert "categorization function"
    This reverts commit ae43095cc0a37af671396e32a78d5bdafdb7fef4.

--- web_scrap.py
+++ web_scrap.py
@@ -4,7 +4,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
-from selenium.webdriver.chrome.options import Options
 url_1 = "https://www.hktvmall.com/hktv/zh/%E8%B6%85%E7%B4%9A%E5%B7%BF%E5%A0%B4/%E8%B6%85%E7%B4%9A%E5%B8%82%E5%A0%B4/%E9%87%8D%E9%87%8F%E7%B4%9A%E6%8E%A8%E4%BB%8B/%E6%B0%B4%E6%9E%9C-%E8%94%AC%E8%8F%9C/main/search?q=%3Arelevance%3Astreet%3Amain%3Acategory%3AAA11030500001"
 url_2 = "https://www.adidas.com.hk/?gclid=Cj0KCQiA4aacBhCUARIsAI55maGm-unelpPysvpPAh_IOH7QZ6bkTznwgCzN8fyMNLXWeCRKfkKM8cMaAsEwEALw_wcB&gclsrc=aw.ds"
 url_3 = "https://docs.google.com/forms/d/e/1FAIpQLSfBQQF14CCzq4c_mRDDNZSQMcobJBX4fOqXZ1bkeYSvs5iPnw/viewform"
@@ -14,19 +13,17 @@ username = "hugofyptest@gmail.com"
 password = "FYP4400test"
 
 def main(link):
-#    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    chrome_options = Options()
-    chrome_options.add_extension("GoFullPageFull-Page-Screen-Capture.crx")
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chrome_options="--headless"
     driver.get(link)
     html = driver.page_source
     driver.maximize_window()
 
-#    for Google forms may require login, work-in-progress
+#    for google forms may require login, work-in-progress
 #    username_input = driver.find_element("IdentifierID")
 #    login = driver.find_element(By.ID("IdentifierNext"))
 #    username_input.send_keys(username)
-#    login.click()
+#   login.click()
 #    password_input = driver.find_element(By.NAME("Passwd"))
 #    password_input.send_keys(password)
 #    login = driver.find_element(By.ID("passwordNext"))
@@ -42,14 +39,14 @@ def main(link):
         sleep(5)
         driver.get_screenshot_as_file("test.png")
         print("Saved as: "+"test.png")
-#    driver.close()
+    driver.close()
     soup = bs4.BeautifulSoup(html,"html.parser")
     products = soup.find("div",{"role":"list"}).find_all("div",{"role":"listitem"})
     for product in products:
-        print(product.find("span",{"class":"M7eMe"}).text)
+        print(product.find("span").text)
 
 #       read category from WordList by read text files
 #          change the output from printing labels to categorising labels
 
-#if __name__ == "__main__":
-main(url_3)
+if __name__ == "__main__":
+    main(url_4)
