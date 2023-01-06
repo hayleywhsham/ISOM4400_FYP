124dd9238a21b0b96acba2e6f810034a58840bfe
hayleysham <hayley11sham@gmail.com>
2023-01-06 16:10:47 +0800

    reset Max from_date when to_date is changed

--- MarketingWebsiteReportGenerationSystem.py
+++ MarketingWebsiteReportGenerationSystem.py
@@ -22,9 +22,12 @@ class MainWindow(QMainWindow):
         self.ui.input_search_page_from_date.setMaximumDate(today_date)
 
         # Connect Function: only connect once for each button
-
         self.ui.button_search_page_search_marketing_sites.clicked.connect(self.search_urls)
         self.ui.input_search_page_from_fb_page.returnPressed.connect(self.ui.button_search_page_search_marketing_sites.click)
+        # reset Max from_date when to_date is changed
+        self.ui.input_search_page_to_date.dateChanged.connect(lambda: self.ui.input_search_page_from_date.setMaximumDate(self.ui.input_search_page_to_date.date()))
+
+
 
     def search_urls(self):
 
@@ -57,6 +60,10 @@ class MainWindow(QMainWindow):
             self.ui.table_links_page_link_list.setItem(rowPosition, 1, QTableWidgetItem(url_dict["Source"]))
             self.ui.table_links_page_link_list.setItem(rowPosition, 2, QTableWidgetItem(url_dict["PostTime"]))
             self.ui.table_links_page_link_list.setItem(rowPosition, 3, QTableWidgetItem(url_dict["ShortLink"]))
+
+    # def to_date_changed(self, new_to_date):
+
+
 
 def main():
     app = QApplication(sys.argv)
