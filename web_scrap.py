eb38c4a9e9b966b124a8569714e362550cf36a70
hugoho82 <hohugo56@gmail.com>
2023-01-26 23:27:06 +0800

    update (26/1)

--- web_scrap.py
+++ web_scrap.py
@@ -17,20 +17,19 @@ url_2 = "https://www.adidas.com.hk/?gclid=Cj0KCQiA4aacBhCUARIsAI55maGm-unelpPysv
 url_3 = "https://docs.google.com/forms/d/e/1FAIpQLSfBQQF14CCzq4c_mRDDNZSQMcobJBX4fOqXZ1bkeYSvs5iPnw/viewform"
 url_4 = "https://forms.gle/PxHxoVDDsgvmftg98"
 url_5 = "https://vitasoy.vitavitasoy.com/sansuiluckydraw2022/index.php/tc/registration"
+url_6 = "https://www.parknshop.com/zh-hk/%E4%BB%8A%E6%9C%9F%E6%8E%A8%E4%BB%8B/c/pnspromo2?utm_medium=PNS_QR&amp;amp;utm_source=FB&amp;amp;utm_campaign=20220701_50Yluckydraw&fbclid=IwAR3pyFRwDdFizxbz4PmRBoD4c8CsnHE1L7_VaAURjeCa0bw9XQ-DDU-9hSU"
 
 username = "hugofyptest@gmail.com"
 password = "FYP4400test"
 
 
-def main(link):
+def web_scrape(link):
 #   driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     chrome_options = Options()
-#    chrome_options.add_extension("GoFullPageFull-Page-Screen-Capture.crx")
-    chrome_options.add_argument("start-maximized")
+#    chrome_options.add_argument("start-maximized")
+    chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     driver.get(link)
-#    driver.switch_to.window(driver.window_handles[1])
-#    driver.close()
     driver.implicitly_wait(10)
     page_rect = driver.execute_cdp_cmd('Page.getLayoutMetrics', {})
     screenshot_config = {'captureBeyondViewport': True,
@@ -44,17 +43,41 @@ def main(link):
     base_64_png = driver.execute_cdp_cmd('Page.captureScreenshot', screenshot_config)
     with open("test.png", "wb") as fh:
         fh.write(base64.urlsafe_b64decode(base_64_png['data']))
-    sleep(2)
-
     try:
         soup = BeautifulSoup(driver.page_source, "html.parser")
+        for script in soup(["script", "style"]):
+            script.extract()
         text = soup.get_text()
         lines = (line.strip() for line in text.splitlines())
         chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
         text = '\n'.join(chunk for chunk in chunks if chunk)
+        text_list = text.split("\n")
     except Exception:
         print(Exception)
-    return text
+    try:
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        link_list = []
+        for links in soup.find_all("a"):
+            link_list.append(links.get("href"))
+    except Exception:
+        print(Exception)
+    print(text_list)
+    print(link_list)
+    sleep(10)
+    return text_list, link_list
 
 
-main(url_1)
+url_list = [url_1]
+for url in url_list:
+    print(url)
+    web_scrape(url)
+    print()
+
+
+# TODO exception: Google login - Google blocked selenium controlled browser login
+# TODO Some text no line breaks not easy to categorise
+# TODO too many irrelevant text (especially shopping sites) will go through categorisation? waste so much time | if not how choose useful info
+# TODO UI
+# TODO screenshot separate files (set directory or UI?)
+
+
