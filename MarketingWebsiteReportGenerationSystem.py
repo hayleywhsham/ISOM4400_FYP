3dd0f80945776b674a2caa17ebfcefa709faf633
hayleysham <hayley11sham@gmail.com>
2023-03-24 21:43:28 +0800

    Fix method for fb post scrap
    allow scrap post up to 236 post (test with nintendo page)
    add cookies for fb scrapper
    Issue:
    cookie need to be updated from time to time
    Will be soft banned :D

--- MarketingWebsiteReportGenerationSystem.py
+++ MarketingWebsiteReportGenerationSystem.py
@@ -28,7 +28,7 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.categoryList = CategoryList()
         self.columnWidgets = []
-        self.url_pool = set()
+        self.url_pool = []
         self.export_info = []
 
         self.ui = Ui_MainWindow()
@@ -106,27 +106,23 @@ class MainWindow(QMainWindow):
         fb_page_name = fb_page_name
         source = "Facebook"
         for post in get_posts(fb_page_name,
-                              pages=10,
+                              pages=99999,
 
                               # Will be blocked easily by Facebook, Facebook API highly restricted
                               # Without cookie can only get 60 newest posts from page
                               # Source : https://developers.facebook.com/docs/graph-api/overview/rate-limiting/
 
-                              # cookies="./fbUserToken.json",
-                              options={
-                                  "posts_per_page": 20
-                              }):
-            post_time = post['time']
-            # print("Post Time:",type(post['time'])) print(f'data: start_date:{start_date},post_time: {post_time},
-            # end_date: {end_date}, \nlogic check {post_time < start_date}, {post_time <= end_date}, \npost text : {
-            # post["text"][:10]}\n')
-            if post_time.date() < start_date:
-                break
-            if post_time.date() <= end_date:
-                urls = set(get_all_url_from_string(post['text']))  # set: unique per post
-                self.url_pool.update(urls)
-                # print(urls)
-                for url in urls:
+                               cookies="./fbUserToken.json",):
+
+            url  = post['link']
+            if url and url not in self.url_pool:
+                post_time = post['time']
+
+                if post_time.date() <= end_date and post_time.date() >= start_date :
+                    # urls = set(get_all_url_from_string(post['text']))  # set: unique per post
+                    # self.url_pool.update(urls)
+                    # print(urls)
+                    self.url_pool.append(url)
                     row_position = self.ui.table_links_page_link_list.rowCount()
                     self.ui.table_links_page_link_list.insertRow(row_position)
                     self.ui.table_links_page_link_list.setItem(row_position, 0, QTableWidgetItem(fb_page_name))
@@ -139,6 +135,8 @@ class MainWindow(QMainWindow):
                                              source,
                                              post_time.strftime("%Y/%m/%d"),
                                              url])
+                else:
+                    break
 
         last_update_time = datetime.datetime.now().strftime("%Y/%m/%d %H:%M")
         self.ui.lbl_links_page_last_updated_datetime.setText(last_update_time)
--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-a = [1,2,3]
-a.remove(4,True)
-print(a)
+# link = "https://graph.facebook.com/v16.0/me?fields=id,name&access_token=EAAIOCSeJx44BANw6jt2ZAZCXPuE6fc44xXtmZACrDYIBZAppyQiYoXv8oc8OlaapTf12t1ZAI4MqjEqBVdJjk2cPKADaurNWpPd5v4VOoWURltT8RMoaPZANpfgwCI3zmS3ZB1Il61c4xcnfP1W8G0GO0coiqAa6E57J7L7ZCJTjcyNcVqm7qZAuOv8MjrVGhMJLEnuPKZBgw17hv68R5M1pMZBOKRrw35z0MSMJMETYQcNk93YgCiBaWG9"
+#
+# import requests
+# import json
+# data = json.loads(str(requests.get(link).content)[2:-1])
+# print(data)
+# print(data["id"])
+# print(data["name"])
+
+from facebook_scraper import get_posts
+
+for count,post in enumerate(get_posts('nintendo', pages=999, cookies="./fbUserToken.json",)):
+     print(f'Index: {count}, link:[{post["link"]}]')
