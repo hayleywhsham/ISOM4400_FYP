d4b05dc08dae7ac19d3eda66d8fce5fba0d2d83d
hugoho82 <hohugo56@gmail.com>
2023-04-15 23:10:54 +0800

    15/4 (23:10pm) fixed NoneType error for fb_scrapper_with_dict

--- fb_scraper_with_dict.py
+++ fb_scraper_with_dict.py
@@ -12,24 +12,25 @@ def get_all_url_from_string(string) -> list[str]:
     end_target_list = ["\n", " ", "更多", "，"]
     end_pos = 0
 
-    while end_pos < len(string):
-        pos_list = []
-        string = string[end_pos:]
-        for target in target_list:
-            if (string.find(target) != -1):
-                pos_list.append(string.find(target))
+    if string:
+        while end_pos < len(string):
+            pos_list = []
+            string = string[end_pos:]
+            for target in target_list:
+                if (string.find(target) != -1):
+                    pos_list.append(string.find(target))
 
-        if not (len(pos_list)):  # break if no URLs found
-            return urls
+            if not (len(pos_list)):  # break if no URLs found
+                return urls
 
-        string = string[min(pos_list):]
-        pos_list = []
+            string = string[min(pos_list):]
+            pos_list = []
 
-        for target in end_target_list:
-            if (string.find(target) != -1):
-                pos_list.append(string.find(target))
-        end_pos = min(pos_list) if len(pos_list) else len(string)
-        urls.append(string[:end_pos])
+            for target in end_target_list:
+                if (string.find(target) != -1):
+                    pos_list.append(string.find(target))
+            end_pos = min(pos_list) if len(pos_list) else len(string)
+            urls.append(string[:end_pos])
 
     return urls
 
